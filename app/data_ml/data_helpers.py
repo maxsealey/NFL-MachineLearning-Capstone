d7d819a6436565619979279cac3b5e3750888dcf
Max Sealey <maxsealey@users.noreply.github.com>
2024-06-08 01:58:34 -0700

    Modularized core bits of functionality by creating data_helpers

---
+++ app/data_ml/data_helpers.py
@@ -0,0 +1,71 @@
+"""
+data_helpers.py
+Author: Max Sealey
+
+Utility functions
+"""
+import pandas as pd
+import sqlite3 as sql
+
+"""
+get_df_with_cleaned_data()
+
+Args: sql script (string)
+Ret: Dataframe with data 
+
+
+Called in visualize_data.py and ml_predict
+"""
+def get_df_with_cleaned_data(script):
+    # loads data to pandas dataframe
+    csv = '../../data/nfl_data.csv'
+    df = pd.read_csv(csv)
+
+    # connect to database
+    conn = sql.connect('nfl_data.db')
+    cursor = conn.cursor()
+    df.to_sql('nfl_data', conn, if_exists='replace', index=False)
+    conn.commit()
+
+    # cleaning and preparing data by creating a new table with only relevant information
+    cursor.executescript(script)
+    conn.commit()
+
+    q = 'SELECT * FROM relevant_data'
+    df = pd.read_sql_query(q, conn)
+    conn.close()
+    return df
+
+
+"""
+sb_or_playoffs()
+
+Args: team name (string), year (int), playoffs_bool (int, 1 or 0), sb_bool (int, 1 or 0)
+Ret: Statement (string)
+
+Called in visualize_data.py
+"""
+def sb_or_playoffs(team, year, playoffs_bool, sb_bool):
+    won_sb = f'The {team} { "won" if sb_bool else "did not win"} the Super Bowl in {year}.'
+    lost_in_playoffs = f'The {team} lost at some point in the playoffs in {year}.'
+    did_not_make_playoffs = f'The {team} did not make the playoffs in {year}.'
+
+    if sb_bool:
+        return won_sb
+    elif playoffs_bool:
+        return lost_in_playoffs
+    else:
+        return did_not_make_playoffs
+
+
+"""
+position_labels()
+
+Args: N/A
+Ret: List of easily readable position names
+
+Called in visualize_data.py
+"""
+def position_labels():
+    return ['QB', 'RB', 'WR', 'TE', 'OL', 'IDL', 'EDGE', 'LB', 'S', 'CB']
+
--- app/data_ml/ml_predict.py
+++ app/data_ml/ml_predict.py
@@ -1,99 +1,79 @@
-import sqlite3 as sql
+"""
+ml_predict.py
+Author: Max Sealey
+
+Uses Machine Learning (a Supervised Learning Random Forest Classification model)
+to make a prediction on the playoff likelihood of teams based on salary cap data
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
+import data_helpers as util
+from sql_scripts import retrieve_prediction_data_script
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.preprocessing import StandardScaler
 
-# loads data to pandas dataframe
-csv = 'nfl_data.csv'
-df = pd.read_csv(csv)
+"""
+make_prediction()
 
-# connect to database
-conn = sql.connect('../../data/nfl_data.db')
-cursor = conn.cursor()
+Args: N/A
+Ret: N/A
 
-# convert dataframe to sql table
-df.to_sql('nfl_data', conn, if_exists='replace', index=False)
+Creates an ML model for predicting whether a team makes the playoffs or not
+based on salary cap data (classification: playoffs or not)
 
-conn.commit()
+Prints accuracy score and classification report to the console, 
+displays confusion matrix showing actual vs predicted results on test data
+"""
+def make_prediction():
+    # Passes in sql script into utility function get_df_with_cleaned_data() to retrieve
+    # dataframe with only the data needed to make a prediction
+    df = util.get_df_with_cleaned_data(retrieve_prediction_data_script())
 
-# cleaning and preparing data by creating a new table with only relevant information
-new_table_script = '''
-  DROP TABLE IF EXISTS relevant_data;
+    # features are % of cap allocated to qb, offense (as a whole), and defense (as a whole)
+    features = ['qb_p', 'off_p', 'def_p']
+    X = df[features]
+    y = df['playoffs']
 
-  CREATE TABLE relevant_data (
-    team VARCHAR(25),
-    season INT,
-    playoffs INT,
-    qb_p NUMERIC(2, 8),
-    off_p NUMERIC(2, 8),
-    def_p NUMERIC(2, 8)
-  );
+    # split data into training and testing subsets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-    INSERT INTO relevant_data (
-    team,
-    season,
-    playoffs,
-    qb_p,
-    off_p,
-    def_p
-  )
-  SELECT team, season, playoffs, qb_p, offense_p, defense_p 
-  FROM nfl_data
-  ORDER BY team;
+    # initialize StandardScaler object, fit and transform the data
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-'''
+    # initialize RFC model and fit to training data
+    model = RandomForestClassifier()
+    model.fit(X_train_scaled, y_train)
 
-cursor.executescript(new_table_script)
-conn.commit()
+    # predicts labels on the scaled test set and stores in y_pred
+    y_pred = model.predict(X_test_scaled)
 
-# reading relevant data into dataframe to be used in ML model
-q = 'SELECT * FROM relevant_data'
-df = pd.read_sql_query(q, conn)
+    # compares predictions vs test results
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    class_report = classification_report(y_test, y_pred)
 
-conn.close()
+    # Display accuracy score and classification report
+    print(f'Accuracy: {accuracy:.2f}\n')
+    print('Classification Report:\n', class_report)
+    results_df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+    results_df['Correct Prediction'] = results_df['Actual'] == results_df['Predicted']
+    print("")
+    print("Actual vs Predicted Outcomes:")
+    print(results_df.head())
+    print("")
 
-# features are % of cap allocated to qb, offense (as a whole), and defense (as a whole)
-features = ['qb_p', 'off_p', 'def_p']
-X = df[features]
-y = df['playoffs']
+    # confusion matrix visualization
+    sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', xticklabels=['No Playoffs', 'Playoffs'],
+                yticklabels=['No Playoffs', 'Playoffs'])
+    plt.xlabel('Predicted Outcomes')
+    plt.ylabel('Actual Outcomes')
+    plt.title('NFL Playoff Predictor Confusion Matrix')
+    plt.show()
 
-# split data into training and testing subsets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# initialize StandardScaler object, fit and transform the data
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
-# initialize RFC model and fit to training data
-model = RandomForestClassifier()
-model.fit(X_train_scaled, y_train)
-
-# predicts labels on the scaled test set and stores in y_pred
-y_pred = model.predict(X_test_scaled)
-
-# compares predictions vs test results
-accuracy = accuracy_score(y_test, y_pred)
-conf_matrix = confusion_matrix(y_test, y_pred)
-class_report = classification_report(y_test, y_pred)
-
-print(f'Accuracy: {accuracy:.2f}\n')
-print('Classification Report:\n', class_report)
-results_df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-results_df['Correct Prediction'] = results_df['Actual'] == results_df['Predicted']
-print("")
-print("Actual vs Predicted Outcomes:")
-print(results_df.head(20))
-print("")
-
-# confusion matrix visualization
-sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', xticklabels=['No Playoffs', 'Playoffs'], yticklabels=['No Playoffs', 'Playoffs'])
-plt.xlabel('Predicted Outcomes')
-plt.ylabel('Actual Outcomes')
-plt.title('NFL Playoff Predictor Confusion Matrix')
-plt.show()
+make_prediction()
